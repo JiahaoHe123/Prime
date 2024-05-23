24dbbff52d80ad7f6cd41b3bedec8c49665e92b6
JiahaoHe123 <hejh0612@gmail.com>
2024-05-23 16:34:46 -0700

    Update primeMain.java

--- primeMain.java
+++ primeMain.java
@@ -1,6 +1,6 @@
 public class primeMain {
   public static void main(String[] args) {
-    prime number = new prime(32);
+    Prime number = new Prime(32);
 //     HashMap<Integer, Integer> factorization = number.primeFactorization();
 //     int phi = number.findPhi(factorization);
     System.out.println(number.numberWithOrder(32).toString());
